eaef94e962a1023188d1400777aac8bfee79f439
soukaina-sta <75736345+soukaina-sta@users.noreply.github.com>
2023-04-04 02:11:10 +0000

    Add files via upload

--- mpi-week/assignments/MatrixVectorMult.py
+++ mpi-week/assignments/MatrixVectorMult.py
@@ -28,15 +28,15 @@ def matrixVectorMult(A, b, x):
         for j in range(col):
             x[i] += a[j] * b[j]
 
-    return 0
+    return x
 
 ########################initialize matrix A and vector b ######################
 #matrix sizes
 SIZE = 1000
-#Local_size = 
+Local_size = SIZE // nbOfproc
 
 # counts = block of each proc
-#counts = 
+counts = [Local_size] * nbOfproc
 
 if RANK == 0:
     A = lil_matrix((SIZE, SIZE))
@@ -52,11 +52,14 @@ else :
 
 
 #########Send b to all procs and scatter A (each proc has its own local matrix#####
-#LocalMatrix = 
+LocalMatrix = np.zeros((Local_size, Local_size)) 
+Localb = np.zeros(Local_size)
 # Scatter the matrix A
+displacements = [sum(counts[:i]) for i in range(nbOfproc)]
+COMM.Scatterv([A,counts,displacements,MPI.DOUBLE],LocalMatrix,root =0)
 
 #####################Compute A*b locally#######################################
-#LocalX = 
+LocalX = np.zeros(Local_size)
 
 start = MPI.Wtime()
 matrixVectorMult(LocalMatrix, b, LocalX)
@@ -66,19 +69,20 @@ if RANK == 0:
 
 ##################Gather te results ###########################################
 # sendcouns = local size of result
-#sendcounts = 
-# if RANK == 0: 
-#     X = ...
-# else :
-#     X = ..
+sendcounts = [Local_size] * nbOfproc
+sendcounts[-1] += SIZE * nbOfproc
+if RANK == 0: 
+     X = np.zeros(SIZE)
+else :
+     X = None
 
 # Gather the result into X
-
+COMM.Gatherv(LocalX,[X,sendcounts,displacements, MPI.DOUBLE],root = 0)
 
 ##################Print the results ###########################################
 
 if RANK == 0 :
     X_ = A.dot(b)
     print("The result of A*b using dot is :", np.max(X_ - X))
-    # print("The result of A*b using parallel version is :", X)
+    print("The result of A*b using parallel version is :", X)
     
